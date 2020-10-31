bd476b91e009e69ec8a0bcaf5295527e7a3e7129
Gabriel <gaba929@gmail.com>
2020-10-31 13:34:26 -0600

    preapre CMSSW

--- prepare.sh
+++ prepare.sh
@@ -9,9 +9,28 @@ fi
 cd CMSSW_10_2_20_UL/src
 eval `scram runtime -sh`
 
-pyfile="BPH-fragment.py"
-#pyfile="fragment.py"
-#pyfile="fragment.py"
+# Configuration parameters
+
+fargment_pyfile="BPH-fragment.py"
+#fargment_pyfile="fragment.py"
+#fargment_pyfile="fragment.py"
+
+run_configfile="BPH-run.py"
+#run_configfile="BPH-run.py"
+#run_configfile="BPH-run.py"
+
+result_root="BPH-result.root"
+#result_root="BPH-result.root"
+#result_root="BPH-result.root"
+
+EVENTS=250000
+
 
 # Download fragment from myGitHub
-curl -s -k https://raw.githubusercontent.com/gaas92/MCgenScripts/master/$pyfile --retry 3 --create-dirs -o Configuration/GenProduction/python/BPH-RunIIFall18GS-00251-fragment.py
+curl -s -k https://raw.githubusercontent.com/gaas92/MCgenScripts/master/$fargment_pyfile --retry 3 --create-dirs -o Configuration/GenProduction/python/$fargment_pyfile
+
+scram b
+cd ../../
+
+# cmsDriver command
+cmsDriver.py Configuration/GenProduction/python/$fargment_pyfile --eventcontent RAWSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --conditions 102X_upgrade2018_realistic_v11 --beamspot Realistic25ns13TeVEarly2018Collision --step GEN --geometry DB:Extended --era Run2_2018 --python_filename $run_configfile --fileout file:$result_root --no_exec --mc -n $EVENTS || exit $? 
